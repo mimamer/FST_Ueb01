10ce86ccdbf0ec3bd8e1df4b9a6dbe55de0d4bbb
michelle <mersub.trier@gmail.com>
2022-11-07 07:13:24 +0100

    weiteren Test eingefügt

--- src/test/java/DataTest.java
+++ src/test/java/DataTest.java
@@ -1,3 +1,5 @@
+//Michelle Mergen - 1293151
+
 import de.unitrier.st.core.CountryDataPoint;
 import de.unitrier.st.core.DataLoader;
 import de.unitrier.st.core.DataParser;
@@ -13,6 +15,13 @@ public class DataTest {
     @Test
     public void tmpTest() {
         Assert.assertEquals(true, true);
+    }
+    @Test
+    public void dataLoaderFNFExceptionWorks(){
+        DataLoader dataLoader = new DataLoader();
+        String json = dataLoader.loadJsonFile("fileNotFound.json");
+        Assert.assertEquals("",json); //easy to trigger a fail if someone wants to test the test
+
     }
 
 }
